3c40f0f6326f005203820484e6c0426f274f11c4
anchounio <toni.clemente.garcia@gmail.com>
2022-02-14 17:50:23 +0100

    Added styles to user component.

--- src/components/core/user.js
+++ src/components/core/user.js
@@ -1,8 +1,16 @@
+import './user.scss';
+
 export function User() {
     return (
         <div className="app-user">
-            <img src="./public/assets/pepe.png" alt="el pepe" />
-            <span>Pepe</span>
+            <span className="app-user__container-image">
+                <img
+                    className="app-user__container-image__image"
+                    src="./assets/pepe.png"
+                    alt="el pepe"
+                />
+            </span>
+            <span className="app-user__user-name">Pepe</span>
         </div>
     );
 }
