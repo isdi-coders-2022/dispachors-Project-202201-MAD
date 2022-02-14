c2818498414bbd520df482e761305e78ba9451d0
Antonio Morga <anotniomc9719@gmail.com>
2022-02-14 20:29:47 +0100

    Added styles to the menu and included imports to the component

--- src/components/core/menu.js
+++ src/components/core/menu.js
@@ -1,13 +1,19 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHouse,
+    faMagnifyingGlass,
+    faBookmark,
+} from '@fortawesome/free-solid-svg-icons';
+
+import './menu.scss';
+
 export function Menu() {
     return (
         <div className="menu">
-            <i role="menuitem" className="fa-regular fa-house menu__item" />
-            <i
-                role="menuitem"
-                className="fa-regular fa-magnifying-glass menu__item"
-            />
-            <i role="menuitem" className="fa-regular fa-bookmark menu__item" />
+            <FontAwesomeIcon icon={faHouse} />
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FontAwesomeIcon icon={faBookmark} />
         </div>
     );
 }
