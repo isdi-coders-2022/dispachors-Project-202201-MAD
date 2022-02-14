f2974dca2b0e76087463530f6343f74fa34a57aa
anchounio <toni.clemente.garcia@gmail.com>
2022-02-14 18:13:56 +0100

    Fixed user BEM and styles

--- src/components/core/user.js
+++ src/components/core/user.js
@@ -3,13 +3,12 @@ import './user.scss';
 export function User() {
     return (
         <div className="app-user">
-            <span className="app-user__container-image">
-                <img
-                    className="app-user__container-image__image"
-                    src="./assets/pepe.png"
-                    alt="el pepe"
-                />
-            </span>
+            <img
+                className="app-user__image"
+                src="./assets/pepe.png"
+                alt="el pepe"
+            />
+
             <span className="app-user__user-name">Pepe</span>
         </div>
     );
