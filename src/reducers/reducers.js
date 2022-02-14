be33ec6eef1f1a65b43eb3dd34d0e1e9504972b7
anchounio <toni.clemente.garcia@gmail.com>
2022-02-14 12:43:34 +0100

    Created reducers.js file

---
+++ src/reducers/reducers.js
@@ -0,0 +1,23 @@
+import { actionTypes } from './action-types';
+
+export function Reducer(state, action) {
+    switch (action.type) {
+        case actionTypes.load:
+            return [...action.books];
+
+        case actionTypes.add:
+            return [...state, action.book];
+
+        case actionTypes.remove:
+            return state.filter((item) => item.id !== action.book.id);
+
+        case actionTypes.update:
+            return state.map((item) =>
+                item.id === action.book.id
+                    ? { ...item, isRead: !item.isRead }
+                    : item
+            );
+        default:
+            return state;
+    }
+}
