c2b14e760fdccce20168cc78b662081f14558222
Antonio Morga <anotniomc9719@gmail.com>
2022-02-14 13:09:20 +0100

    Created dumb menu component

---
+++ src/components/core/menu.js
@@ -0,0 +1,9 @@
+export function Menu() {
+    return (
+        <div className="menu">
+            <i className="fa-regular fa-house menu__item" />
+            <i className="fa-regular fa-magnifying-glass menu__item" />
+            <i className="fa-regular fa-bookmark menu__item" />
+        </div>
+    );
+}
