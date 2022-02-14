cce1a5d5090839528608eff500284f303bbce457
Antonio Morga <anotniomc9719@gmail.com>
2022-02-14 17:26:32 +0100

    Created api functions in services

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
-export function App() {
+import { getFromCategory } from './services/PRH-api';
+
+export async function App() {
+    const catBooks = await getFromCategory(93);
+    console.log(catBooks);
+
     return <div className="app" />;
 }
---
+++ src/services/PRH-api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const getFromCategory = (catID) => {
+    const URL = `https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/categories/${catID}/titles?rows=5&showCovers=true&sort=random&api_key=mdmzpbe68gz2cc23pc7dhs28`;
+
+    return axios.get(URL);
+};
+
+export const getFromSaved = () => {
+    const URL = `http://localhost:4500/books/`;
+    return axios.get(URL);
+};
+
+export const saveBook = (book) => {
+    const URL = `http://localhost:4500/books/`;
+
+    return axios.post(URL, book);
+};
+
+export const deleteBook = (book) => {
+    const URL = `http://localhost:4500/books/${book.id}`;
+
+    return axios.delete(URL, book.id);
+};
+
+export const updateBook = (book) => {
+    const URL = `http://localhost:4500/books/`;
+
+    return axios.patch(URL, book);
+};
