57b631f853567972640071f36f4a0e0788c172bf
Antonio Morga <anotniomc9719@gmail.com>
2022-02-14 13:18:38 +0100

    Created basic rendering test for Menu component

---
+++ src/components/core/menu.spec.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { Menu } from './menu';
+
+describe('Given the component Menu', () => {
+    describe('When rendering it', () => {
+        test('Then it should appear on the screen', () => {});
+        render(<Menu />);
+        expect(screen.getAllByRole('menuitem')).toHaveLength(3);
+    });
+});
