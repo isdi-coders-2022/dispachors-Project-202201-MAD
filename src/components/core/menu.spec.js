488cd5d160acc8a85b0c9670570090f184b0e4de
Antonio Morga <anotniomc9719@gmail.com>
2022-02-15 10:32:46 +0100

    Fixed sonarqube codesmell

--- src/components/core/menu.spec.js
+++ src/components/core/menu.spec.js
@@ -3,8 +3,9 @@ import { Menu } from './menu';
 
 describe('Given the component Menu', () => {
     describe('When rendering it', () => {
-        test('Then it should appear on the screen', () => {});
-        render(<Menu />);
-        expect(screen.getAllByRole('menuitem')).toHaveLength(3);
+        test('Then it should appear on the screen', () => {
+            render(<Menu />);
+            expect(screen.getAllByRole('menuitem')).toHaveLength(3);
+        });
     });
 });
