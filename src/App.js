cda843d9cb3e090aee4e3af5958e7f99ce9702d4
anchounio <toni.clemente.garcia@gmail.com>
2022-02-15 17:07:19 +0100

    Moved randomCategory function to helpers service

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Category } from './components/category';
 import { Header } from './components/core/header';
 import { Menu } from './components/core/menu';
+import { randomCategories } from './services/helpers';
 
 export function App() {
     return (
@@ -13,29 +14,3 @@ export function App() {
         </div>
     );
 }
-
-const categories = [
-    { id: 2000101307, catName: 'History' },
-    { id: 3000000792, catName: 'Computers' },
-    { id: 90000140898, catName: 'C. G. Jung' },
-    { id: 2000070138, catName: 'Romance' },
-    {
-        id: 3000004583,
-        catName: 'Young Adult Fiction - Comics & Graphic Novels - Horror',
-    },
-    { id: 316, catName: 'Poetry' },
-    { id: 93, catName: 'Drama' },
-    { id: 201, catName: 'Art' },
-    { id: 202, catName: 'Award winners' },
-    { id: 225, catName: 'Mythology and Folklore' },
-];
-
-const randomCategories = () => {
-    const randomCat = [...categories];
-    for (let i = 0; i < 7; i += 1) {
-        const aleatorio = Math.floor(Math.random() * randomCat.length);
-        const numRandom = randomCat[aleatorio];
-        randomCat.splice(aleatorio, 1);
-    }
-    return randomCat;
-};
