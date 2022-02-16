117af9379a02ebb0548e53072963e68026cc8e56
Antonio Morga <anotniomc9719@gmail.com>
2022-02-16 16:51:12 +0100

    Tests are on the way

--- src/components/home/category.spec.js
+++ src/components/home/category.spec.js
@@ -2,6 +2,7 @@
 import { render, screen } from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import { getFromCategory } from '../../services/PRH-api';
 import { Category } from './category';
 
 const server = setupServer();
@@ -11,31 +12,48 @@ beforeEach(() => server.resetHandlers());
 afterAll(() => server.close);
 
 const item = { id: 123, catName: 'Shakespeare' };
-const URL = `https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/categories/${item.id}/titles?rows=5&showCovers=true&sort=random&api_key=mdmzpbe68gz2cc23pc7dhs28`;
-const mockResponse = [
-    {
-        _links: [
-            {},
+const URL = `https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/categories/123/titles?rows=5&showCovers=true&sort=random&api_key=mdmzpbe68gz2cc23pc7dhs28`;
+const mockResponse = {
+    data: {
+        titles: [
             {
-                rel: 'icon',
-                href: 'https://images.randomhouse.com/cover/9780143128618',
-                method: 'GET',
-                parameters: null,
+                isbn: 9780143128618,
+                _links: [
+                    {},
+                    {
+                        rel: 'icon',
+                        href: 'https://images.randomhouse.com/cover/9780143128618',
+                        method: 'GET',
+                        parameters: null,
+                    },
+                ],
+            },
+            {
+                isbn: 9780143128618,
+                _links: [
+                    {},
+                    {
+                        rel: 'icon',
+                        href: 'https://images.randomhouse.com/cover/9780143128618',
+                        method: 'GET',
+                        parameters: null,
+                    },
+                ],
             },
         ],
     },
-];
+};
 
 describe('Given the component Category', () => {
     describe('When rendering it', () => {
-        test('Then it should appear on the screen', async () => {
+        test('Then it should appear on the screen', () => {
             server.use(
                 rest.get(URL, (req, response, context) =>
                     response(context.status(200), context.json(mockResponse))
                 )
             );
 
-            await render(
+            render(
                 <Category
                     key={item.id}
                     catID={item.id}
@@ -45,5 +63,25 @@ describe('Given the component Category', () => {
 
             expect(screen.getByText(/Shakespeare/i)).toBeTruthy();
         });
+        // test('Then it should also display a book', async () => {
+        //     server.use(
+        //         rest.get(URL, (req, response, context) =>
+        //             response(context.status(200), context.json(mockResponse))
+        //         )
+        //     );
+
+        //     render(
+        //         <Category
+        //             key={item.id}
+        //             catID={item.id}
+        //             categoryName={item.catName}
+        //         />
+        //     );
+
+        //     expect(await (await getFromCategory(item.id)).data).toEqual(
+        //         'something'
+        //     );
+        //     expect(screen.getByAltText(/book/i)).toBeTruthy();
+        // });
     });
 });
