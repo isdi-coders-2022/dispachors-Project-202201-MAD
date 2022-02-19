31e92f4dcbdb2c6d1b087a30688f06c74c4164f3
Sebas Vargas <sebas_10parla@hotmail.com>
2022-02-19 17:56:39 +0100

    Tested removeBook  action

--- src/reducers/reducers.spec.js
+++ src/reducers/reducers.spec.js
@@ -1,14 +1,19 @@
 import * as action from './action-creators';
 import { Reducer } from './reducers';
 
+let mockState = [];
+
 describe('Given the function Reducer', () => {
+    beforeEach(() => {
+        mockState = [
+            { id: 1, bookName: '1984' },
+            { id: 2, bookName: 'Soledad' },
+            { id: 3, bookName: 'Comedia' },
+        ];
+    });
+
     describe('When passing a state and the add action', () => {
         test('Then it should return the state with the added book', () => {
-            const mockState = [
-                { id: 1, bookName: '1984' },
-                { id: 2, bookName: 'Soledad' },
-                { id: 3, bookName: 'Comedia' },
-            ];
             const newState = Reducer(
                 mockState,
                 action.addBook({ id: 4, bookName: 'Renglones' })
@@ -22,14 +27,16 @@ describe('Given the function Reducer', () => {
             ]);
         });
     });
-    describe('When passing a state and the load action', () => {
-        test('then it should return the current state', () => {
-            const newState2 = Reducer(mockState, action.loadBooks(mockState));
+    describe('When passing a state and the remove action', () => {
+        test('then it should return the state without the removed book', () => {
+            const currentState3 = Reducer(
+                mockState,
+                action.removeBook({ id: 3, bookName: 'Comedia' })
+            );
 
-            expect(newState2).toEqual([
+            expect(currentState3).toEqual([
                 { id: 1, bookName: '1984' },
                 { id: 2, bookName: 'Soledad' },
-                { id: 3, bookName: 'Comedia' },
             ]);
         });
     });
