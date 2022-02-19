c781ac836ce9021987184e9ae1e6bbe38f99c814
anchounio <toni.clemente.garcia@gmail.com>
2022-02-19 17:59:48 +0100

    fixed index test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,17 +1,17 @@
 // src/index.spec.js
 /* eslint-env jest */
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { ReactStrictMode, rootElement } from './index';
+import React from 'react';
+// import { ReactStrictMode, rootElement } from './index';
 
 jest.mock('react-dom', () => ({ render: jest.fn() }));
 
 describe('index.js', () => {
     it('renders without crashing', () => {
-        ReactDOM.render(ReactStrictMode, rootElement);
+        ReactDOM.render(<React.StrictMode />, document.getElementById('root'));
         expect(ReactDOM.render).toHaveBeenCalledWith(
-            ReactStrictMode,
-            rootElement
+            <React.StrictMode />,
+            document.getElementById('root')
         );
     });
 });
